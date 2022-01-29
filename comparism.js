7dbee9f12d0fe338228a2359f3d2735a46a79a2d
Shakilk Sarder <shakil01748@hotmail.com>
2022-01-29 13:15:23 +0600

    conditional

---
+++ comparism.js
@@ -0,0 +1,7 @@
+// console.log(10 > 5);
+// console.log(1 > 5);
+// console.log(5 == 5);
+// console.log(5 != 5);
+// console.log(5 != 6);
+// console.log(5 && 6);
+// console.log(5 || 6);
---
+++ conditional.js
@@ -0,0 +1,18 @@
+// var isFoodReady = true;
+
+// if (isFoodReady == false) {
+//     console.log('I will Eat');
+// }
+// else {
+//     console.log('I will Not Eat');
+// }
+
+var laptopPrice = 70000;
+var myBudget = 50000;
+if (laptopPrice < myBudget) {
+    console.log('I will buy MacBook');
+}
+else {
+    console.log('I will buy HP');
+}
+
