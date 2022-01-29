bb29d31cb9cfadcc2f1de5c1d80fe46122c8f1e9
Shakilk Sarder <shakil01748@hotmail.com>
2022-01-29 23:31:06 +0600

    signal  practice

---
+++ signal.js
@@ -0,0 +1,11 @@
+var signal = 'yellow';
+
+if (signal == 'red') {
+    console.log('Do not Move !! Danager Ahead');
+}
+else if (signal == 'green') {
+    console.log('Can Move !!')
+}
+else {
+    console.log('Stop');
+}
